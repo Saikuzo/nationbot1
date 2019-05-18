0d270bc6ee73446725cc1a7d04344c836eca5921
Saikuzo <41024466+Saikuzo@users.noreply.github.com>
2019-05-18 21:41:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//  Partie Music
+var dispatcher;
+
+// Partie Variable
 const PREFIX = "!";
+
+// Fonction
+
+function sendError(message, description) {
+    message.channel.sendEmbed({embed: {
+        color: 15158332,
+        description: ':x: ' + description 
+    }});
+}
+
+
 
 client.on('ready', () => {
     console.log(`Client connecté sous ${client.user.tag}!`);
@@ -26,6 +41,69 @@ client.on('guildMemberRemove', member => {
         .setDescription(`:small_orange_diamond: Aurevoir ${member}! a plus tard sur le serveur discord de Nation :sob: :call_me:`)
     channel.sendEmbed(embed);
 })
+
+client.on('message', message => {
+    if (message.content.startsWith(PREFIX + "sondage")) {
+        if(message.author.id === "315494374429556737"){
+            let args = message.content.split(" ").slice(1);
+            let thingToEcho = args.join(" ")
+            var embed = new Discord.RichEmbed()
+                .setDescription("Sondage")
+                .addField(thingToEcho, "Répondre avec ✅ ou ❌")
+                .setColor("0xB40404")
+                .setTimestamp()
+            message.guild.channels.find(ch => ch.id === "578308716168871938").sendEmbed(embed)
+            .then(function (message) {
+                message.react("✅")
+                message.react("❌")
+            }).catch(function() {
+            });
+            }else{
+                return message.reply("Tu n'as pas la permission. ")
+            }
+    }
+})
+
+/* client.on('message', message => {
+    if(message.content[0] === PREFIX){
+        let splitMessage = message.content.split(" ");
+        if(splitMessage[0] === '!play') {
+            if(splitMessage.length === 2)
+            {
+                
+                if(message.member.voiceChannel)
+                {
+                    message.member.voiceChannel.join().then(connection => {
+                        dispatcher = connection.playArbitraryInput(splitMessage[1]);
+
+                        dispatcher.on('error', e => {
+                            console.log(e);
+                        });
+
+                        dispatcher.on('end', e => {
+                            dispatcher = undefined;
+                            console.log('Fin du son');
+                        });
+                    }).catch(console.log);
+                }
+                else
+                send.message.channel('Erreur, Rejoingez un salon vocal ! ')
+            }
+            else
+                sendError(message, 'Erreur, problème dans les paramètres');
+        }
+        else if(splitMessage[0] === '!pause') {
+            if(dispatcher !== undefined)
+                dispatcher.pause();
+        }
+        else if(splitMessage[0] === '!resume') {
+            if(dispatcher !== undefined)
+                dispatcher.resume();
+        }
+    }
+});
+
+*/
 
 /* Connexion */
 client.login(process.env.TOKEN);
